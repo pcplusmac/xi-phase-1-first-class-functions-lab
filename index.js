f227901ccf61cdb352613a5e3785eff2432450ed
el-sam <samurai.h@yahoo.com>
2023-10-17 11:49:08 +0800

    successful return first 2 drivers by 'returnFirstTwoDrivers'

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 // Code your solution in this file!
 
+const arr = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+// define function that return first 2 drivers
+function  returnFirstTwoDrivers(arr) {
+    const arrNew = [arr[0],arr[1]];
+    // console.log(arrNew);
+    return (arrNew);
+
+}
+console.log(arr);
+returnFirstTwoDrivers();
+
+
+
+
