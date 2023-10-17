e0579be00ef2de4a98739faf67e338d169736254
el-sam <samurai.h@yahoo.com>
2023-10-17 11:58:41 +0800

    successful return the last 2 elements

--- index.js
+++ index.js
@@ -13,5 +13,12 @@ console.log(arr);
 returnFirstTwoDrivers();
 
 
+//  Define the function return the last 2 drivers
+function returnLastTwoDrivers(arr) {
+    let len = arr.length;
+    const arrNew = [arr[len-2],arr[len-1]];
+    console.log(arrNew);
+    return (arrNew);
 
+} 
 
